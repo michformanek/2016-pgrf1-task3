588b6dfbb83fa8af4e42c5a9fa68a56467d42f0e
Michal Formanek <michformanek@gmail.com>
2016-12-31 20:03:45 +0100

    Add matrix to renderer

--- src/geometry/Renderer.java
+++ src/geometry/Renderer.java
@@ -4,12 +4,14 @@ import java.util.function.Function;
 
 import annotations.NotNull;
 import rasterdata.RasterImage;
+import transforms.Mat4;
 import transforms.Point3D;
 
 public interface Renderer<VertexType, ConnectivityType, PixelType> {
 	@NotNull RasterImage<PixelType> render(
 			@NotNull Solid<VertexType, ConnectivityType> solid, 
 			@NotNull PixelType pixelValue,
+			@NotNull Mat4 finalMatrix,
 			@NotNull RasterImage<PixelType> img,
 			@NotNull Function<VertexType, Point3D> toPoint3D);
 }
--- src/geometry/RendererEdgesTriangles.java
+++ src/geometry/RendererEdgesTriangles.java
@@ -1,14 +1,15 @@
 package geometry;
 
-import java.util.List;
-import java.util.function.Function;
-import java.util.stream.IntStream;
-
 import annotations.NotNull;
 import rasterdata.RasterImage;
 import rasterization.LineRasterizer;
+import transforms.Mat4;
 import transforms.Point3D;
 import transforms.Vec3D;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.IntStream;
 
 public class RendererEdgesTriangles<VertexType, PixelType> implements Renderer<VertexType, Connectivity, PixelType> {
 
@@ -22,6 +23,7 @@ public class RendererEdgesTriangles<VertexType, PixelType> implements Renderer<V
 	public @NotNull RasterImage<PixelType> render(
 			final @NotNull Solid<VertexType, Connectivity> solid, 
 			final @NotNull PixelType pixelValue,
+			final @NotNull Mat4 finalMatrix,
 			final @NotNull RasterImage<PixelType> img, 
 			final @NotNull Function<VertexType, Point3D> toPoint3D) {
 		return solid.getParts().stream()
@@ -32,12 +34,12 @@ public class RendererEdgesTriangles<VertexType, PixelType> implements Renderer<V
 					case EDGE_LIST: 
 						return renderEdgeList(
 								solid.getVertices(), solid.getIndices(),
-								pixelValue, image, toPoint3D,
+								pixelValue,finalMatrix, image, toPoint3D,
 								part.getStartIndex(), part.getPrimitiveCount());
 					case TRIANGLE_LIST: 
 						return renderTriangleList(
 								solid.getVertices(), solid.getIndices(),
-								pixelValue, image, toPoint3D,
+								pixelValue,finalMatrix, image, toPoint3D,
 								part.getStartIndex(), part.getPrimitiveCount());
 					}
 					return image;
@@ -51,18 +53,19 @@ public class RendererEdgesTriangles<VertexType, PixelType> implements Renderer<V
 			final @NotNull List<VertexType> vertices,
 			final @NotNull List<Integer> indices,
 			final @NotNull PixelType pixelValue,
+			final @NotNull Mat4 finalMatrix,
 			final @NotNull RasterImage<PixelType> img, 
 			final @NotNull Function<VertexType, Point3D> toPoint3D,
 			final int startIndex, final int primitiveCount) {
 		return IntStream.iterate(startIndex, i -> i + 2)
 				.limit(primitiveCount)
-				.boxed()	// z IntStream delame Stream<Integer>,// abychom meli nasledujici funkci reduce... TODO: Je tam na to udělátko ;)
+				.boxed()	// z IntStream delame Stream<Integer>,// abychom meli nasledujici funkci reduce...
 				.reduce(img, 
 					(final @NotNull RasterImage<PixelType> image, final Integer i) -> 
 						renderEdge(
 							toPoint3D.apply(vertices.get(indices.get(i))),
 							toPoint3D.apply(vertices.get(indices.get(i + 1))),
-							pixelValue, image
+							pixelValue, finalMatrix,image
 						),
 					(lhs, rhs) -> rhs // ignoruje se pri sekvencnim streamu
 				);
@@ -72,28 +75,54 @@ public class RendererEdgesTriangles<VertexType, PixelType> implements Renderer<V
 			final @NotNull List<VertexType> vertices,
 			final @NotNull List<Integer> indices,
 			final @NotNull PixelType pixelValue,
+			final @NotNull Mat4 finalMatrix,
 			final @NotNull RasterImage<PixelType> img, 
 			final @NotNull Function<VertexType, Point3D> toPoint3D,
 			final int startIndex, final int primitiveCount) {
-		// zde doplnit prochazeni po trech vrcholech - 3x volat renderEdge
-		// renderEdge(..., pixelValue, renderEdge(..., pixelValue, renderEdge(..., pixelValue, image)))
 
-		//TODO
-		return img;
+			return IntStream.iterate(startIndex, i -> i + 3)
+				.limit(primitiveCount)
+				.boxed()
+				.reduce(img,
+						(final @NotNull RasterImage<PixelType> image, final Integer i) ->
+								renderEdge(
+										toPoint3D.apply(vertices.get(indices.get(i))),
+										toPoint3D.apply(vertices.get(indices.get(i + 1))),
+										pixelValue,finalMatrix, renderEdge(
+												toPoint3D.apply(vertices.get(indices.get(i))),
+												toPoint3D.apply(vertices.get(indices.get(i + 2))),
+												pixelValue, finalMatrix, renderEdge(
+														toPoint3D.apply(vertices.get(indices.get(i + 1))),
+														toPoint3D.apply(vertices.get(indices.get(i + 2))),
+														pixelValue, finalMatrix,image
+												)
+										)
+								),
+						(lhs, rhs) -> rhs
+				);
 	}
 
 	private @NotNull RasterImage<PixelType> renderEdge(
 			final @NotNull Point3D p1,
 			final @NotNull Point3D p2,
 			final @NotNull PixelType pixelValue,
+			final @NotNull Mat4 finalMatrix,
 			final @NotNull RasterImage<PixelType> img) {
-		// zde doplnit orezani w, dehomogenizaci, viewport transformaci a vykresleni Linerem
 
-		// 4D - 3D -> dehomogenizace
-		final Vec3D a = p1.dehomog().orElse(new Vec3D()); //Asi by to chtelo nejakou rozumnou vychozi hodnotu, nebo vyhodit vyjimku -> invalid argument?
-		final Vec3D b = p2.dehomog().orElse(new Vec3D());
+			if (p1.getW() <= 0 || p2.getW() <= 0)
+			return img;
 
+
+		final Vec3D a = p1.mul(finalMatrix).dehomog().orElse(new Vec3D()); //invalid argument?
+		final Vec3D b = p2.mul(finalMatrix).dehomog().orElse(new Vec3D());
+
+		if (notInLimits(a) || notInLimits(b))
+			return img;
 
 		return liner.drawLine(img,a.getX(),a.getY(),b.getX(),b.getY(),pixelValue);
-	} 
+	}
+
+	private boolean notInLimits(Vec3D vec3D) {
+		return (vec3D.getX() < -1 || vec3D.getX() > 1) || (vec3D.getY() < -1 || vec3D.getY() > 1) || (vec3D.getZ() < -1 || vec3D.getZ() > 1);
+	}
 }
